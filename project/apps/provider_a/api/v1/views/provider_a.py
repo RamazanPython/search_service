f38575146366c597bda8cb8c6038a3f327964bbf
Rakhimbayev Ramazan <r.rakhimbayev@gmail.com>
2022-08-11 23:17:51 +0600

    Changed method to POST

--- project/apps/provider_a/api/v1/views/provider_a.py
+++ project/apps/provider_a/api/v1/views/provider_a.py
@@ -7,7 +7,7 @@ from provider_a.functions import get_flights_data
 from provider_a.api.v1.serializers import ProviderASerializer
 
 
-@api_view(['GET'])
+@api_view(['POST'])
 def search(request: Request) -> Response:
     flights_data = get_flights_data()
     return Response(ProviderASerializer(flights_data).data, status=status.HTTP_200_OK)
