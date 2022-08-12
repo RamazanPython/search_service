d67f072d11fa492c9334becefdae4302d29cbed3
Rakhimbayev Ramazan <r.rakhimbayev@gmail.com>
2022-08-12 22:21:11 +0600

    Added ExchangeRateService

--- project/apps/airflow/services/exchange_rate.py
+++ project/apps/airflow/services/exchange_rate.py
@@ -1,7 +1,9 @@
 from django.utils import timezone
 
+from bs4 import BeautifulSoup
 from requests.models import Response
 
+from airflow.models import ExchangeRate
 from airflow.requests import exchange_rate_get_request
 from airflow.exceptions import ExchangeRateResponseError
 from utils.exception_consts import EXCHANGE_RATE_RESPONSE_NOT_OK
@@ -26,3 +28,16 @@ class ExchangeRateService:
             fp=None
         )
         raise error
+
+    @staticmethod
+    def save_exchange_rate() -> ExchangeRate:
+        response = ExchangeRateService.get_exchange_rate()
+        xml = BeautifulSoup(response.text.encode(), features='xml')
+        items = xml.find_all('item')
+        data = {}
+        for item in items:
+            currency_code = item.find('title').text
+            denomination = item.find('description').text
+            data[currency_code] = denomination
+
+        return ExchangeRate.objects.create(data=data)
