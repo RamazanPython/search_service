447a60fff0f98d0c61647274337b04d74d747936
Rakhimbayev Ramazan <r.rakhimbayev@gmail.com>
2022-08-12 21:06:19 +0600

    Registered Task

--- project/config/celery.py
+++ project/config/celery.py
@@ -2,9 +2,18 @@ import os
 from django.conf import settings
 
 from celery import Celery
+from celery.schedules import crontab
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings')
 celery_app = Celery('config', broker=settings.CELERY_BROKER_URL)
 celery_app.config_from_object('django.conf:settings')
 celery_app.autodiscover_tasks(settings.INSTALLED_APPS)
+
+
+celery_app.conf.beat_schedule = {
+    'update-exchange-rate-every-day': {
+        'task': 'airflow.tasks.exchange_rate.exchange_rate_task',
+        'schedule': crontab(hour='12', minute='0')
+    },
+}
