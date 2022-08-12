ea4adf5a0ba5f66794d3f56a809c58a3ec2998ab
Rakhimbayev Ramazan <r.rakhimbayev@gmail.com>
2022-08-12 19:48:16 +0600

    Added view search

---
+++ project/apps/airflow/api/v1/views/airflow.py
@@ -0,0 +1,12 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
+
+from airflow.models import SearchResult
+
+
+@api_view(['GET'])
+def search(request: Request) -> Response:
+    search_result = SearchResult.objects.create()
+    return Response({'search_id': search_result.pk}, status=status.HTTP_200_OK)
