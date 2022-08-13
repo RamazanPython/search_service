f033fe7f6d14bb75018ba433ccc2505f561a3261
Rakhimbayev Ramazan <r.rakhimbayev@gmail.com>
2022-08-13 19:19:34 +0600

    Fixed bug

--- project/apps/airflow/tasks/search_data.py
+++ project/apps/airflow/tasks/search_data.py
@@ -7,9 +7,9 @@ from airflow.services import SearchDataService
 
 @shared_task
 def send_request_to_provider_a_task(search_id: int) -> None:
-    SearchDataService(search_id=search_id, url=settings.PROVIDER_A_URL).send_request()
+    SearchDataService(search_id=search_id).send_request(settings.PROVIDER_A_URL)
 
 
 @shared_task
 def send_request_to_provider_b_task(search_id: int) -> None:
-    SearchDataService(search_id=search_id, url=settings.PROVIDER_B_URL).send_request()
+    SearchDataService(search_id=search_id).send_request(settings.PROVIDER_B_URL)
