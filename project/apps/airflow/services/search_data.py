e6efa9f3ae6081aa0dfc97a4ea79ce8677293a66
Rakhimbayev Ramazan <r.rakhimbayev@gmail.com>
2022-08-13 19:17:25 +0600

    Changed SearchDataService

--- project/apps/airflow/services/search_data.py
+++ project/apps/airflow/services/search_data.py
@@ -1,6 +1,7 @@
 import uuid
 
 from requests.models import Response
+from typing import Union
 
 from airflow.models import SearchData
 from utils.consts import SearchResultStatusChoice
@@ -11,29 +12,27 @@ from utils.functions import send_post
 
 class SearchDataService:
 
-    def __init__(self, search_id: uuid.uuid4, url: str) -> None:
+    def __init__(self, search_id: uuid.uuid4) -> None:
         self.search_id = search_id
-        self.url = url
-        self.instance = self._create_instance()
 
-    def _create_instance(self) -> SearchData:
-        return SearchData.objects.create(
-            search_id=self.search_id,
-            url=self.url
-        )
-
-    def send_request(self) -> None:
-        response = send_post(url=self.url)
+    def send_request(self, url: str) -> None:
+        response = send_post(url=url)
         if not response.ok:
-            raise SearchDataRequestException(SEARCH_DATA_RESPONSE_NOT_OK.format(self.url))
+            raise SearchDataRequestException(SEARCH_DATA_RESPONSE_NOT_OK.format(url))
 
         self._create_search_data(response)
 
     def _create_search_data(self, response: Response) -> None:
         if not response.json():
-            self.instance.status = SearchResultStatusChoice.NO_DATA.value
-            self.instance.save(update_fields=['status'])
-
-        self.instance.data = response.json()
-        self.instance.status = SearchResultStatusChoice.COMPLETED.value
-        self.instance.save(update_fields=['data', 'status', 'url'])
+            SearchData.objects.create(
+                search_id=self.search_id,
+                url=response.url,
+                status=SearchResultStatusChoice.NO_DATA.value
+            )
+        else:
+            SearchData.objects.create(
+                search_id=self.search_id,
+                url=response.url,
+                data=response.json(),
+                status=SearchResultStatusChoice.COMPLETED.value
+            )
